df036b3cf4a621f4bce8b5bde8c69c9ef4dbcc10
Reethu Gopireddy <98135627+Reethureddy28@users.noreply.github.com>
2022-11-18 19:15:11 -0600

    Authorization screen

---
+++ auth_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+import 'package:loow/authentication/login.dart';
+import 'package:loow/authentication/register.dart';
+
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.cyan,
+                    Colors.amber,
+                  ],
+                  begin:  FractionalOffset(0.0, 0.0),
+                  end:  FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp,
+                )
+            ),
+          ),
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "Left over on wheels",
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.white,
+              fontFamily: "Lobster",
+            ),
+          ),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.lock, color: Colors.white,),
+                text: "Login",
+              ),
+              Tab(
+                icon: Icon(Icons.person, color: Colors.white,),
+                text: "Register",
+              ),
+            ],
+            indicatorColor: Colors.white38,
+            indicatorWeight: 6,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.amber,
+                  Colors.cyan,
+                ],
+              )
+          ),
+          child: const TabBarView(
+            children: [
+              LoginScreen(),
+              RegisterScreen(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
