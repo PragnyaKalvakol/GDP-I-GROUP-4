81cce30a781c31b23488d1ffca455c6372eab3e3
Pragnya Kalvakol <98135573+PragnyaKalvakol@users.noreply.github.com>
2022-11-18 18:59:26 -0600

    Login dart

---
+++ login.dart
@@ -0,0 +1,155 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:loow/global/global.dart';
+import 'package:loow/mainScreens/home_screen.dart';
+import 'package:loow/widgets/custom_text_field.dart';
+import 'package:loow/widgets/error_dialog.dart';
+import 'package:loow/widgets/loading_dialog.dart';
+
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+
+
+class _LoginScreenState extends State<LoginScreen>
+{
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+
+  formValidation()
+  {
+    if(emailController.text.isNotEmpty && passwordController.text.isNotEmpty)
+    {
+      //login
+      loginNow();
+    }
+    else
+    {
+      showDialog(
+          context: context,
+          builder: (c)
+          {
+            return ErrorDialog(
+              message: "Please write email/password.",
+            );
+          }
+      );
+    }
+  }
+
+
+  loginNow() async
+  {
+    showDialog(
+        context: context,
+        builder: (c)
+        {
+          return LoadingDialog(
+            message: "Checking Credentials",
+          );
+        }
+    );
+
+    User? currentUser;
+    await firebaseAuth.signInWithEmailAndPassword(
+      email: emailController.text.trim(),
+      password: passwordController.text.trim(),
+    ).then((auth){
+      currentUser = auth.user!;
+    }).catchError((error){
+      Navigator.pop(context);
+      showDialog(
+          context: context,
+          builder: (c)
+          {
+            return ErrorDialog(
+              message: error.message.toString(),
+            );
+          }
+      );
+    });
+    if(currentUser != null)
+    {
+      readDataAndSetDataLocally(currentUser!).then((value){
+        Navigator.pop(context);
+        Navigator.push(context, MaterialPageRoute(builder: (c)=> const HomeScreen()));
+      });
+    }
+  }
+
+  Future readDataAndSetDataLocally(User currentUser) async
+  {
+    await FirebaseFirestore.instance.collection("sellers")
+        .doc(currentUser.uid)
+        .get()
+        .then((snapshot) async {
+      await sharedPreferences!.setString("uid", currentUser.uid);
+      await sharedPreferences!.setString("email", snapshot.data()!["sellerEmail"]);
+      await sharedPreferences!.setString("name", snapshot.data()!["sellerName"]);
+      await sharedPreferences!.setString("photoUrl", snapshot.data()!["sellerAvatarUrl"]);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: EdgeInsets.all(15),
+              child: Image.asset(
+                "images/seller.png",
+                height: 270,
+              ),
+            ),
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                CustomTextField(
+                  data: Icons.email,
+                  controller: emailController,
+                  hintText: "Email",
+                  isObsecre: false,
+                ),
+                CustomTextField(
+                  data: Icons.lock,
+                  controller: passwordController,
+                  hintText: "Password",
+                  isObsecre: true,
+                ),
+              ],
+            ),
+          ),
+          ElevatedButton(
+            child: const Text(
+              "Login",
+              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold,),
+            ),
+            style: ElevatedButton.styleFrom(
+              primary: Colors.cyan,
+              padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            ),
+            onPressed: ()
+            {
+              formValidation();
+            },
+          ),
+          const SizedBox(height: 30,),
+        ],
+      ),
+    );
+  }
+}
