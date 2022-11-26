5516df9c9e232a89f683444f98b632e212ddc461
Pragnya Kalvakol <98135573+PragnyaKalvakol@users.noreply.github.com>
2022-11-25 21:08:45 -0600

    Progress_bar_dart

---
+++ progress_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+circularProgress()
+{
+  return Container(
+    alignment: Alignment.center,
+    padding: const EdgeInsets.only(top: 12),
+    child: const CircularProgressIndicator(
+      valueColor: AlwaysStoppedAnimation(
+        Colors.amber,
+      ),
+    ),
+  );
+}
