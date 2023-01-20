17d68d712db0cbcf08ad0fc1270c4bf33272bf9a
Sreeja Kumbhum <98132897+ksreejareddy@users.noreply.github.com>
2023-01-20 17:33:18 -0600

    auth screen

---
+++ auth_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+import 'register.dart';
+
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.amber,
+                    Colors.cyan,
+                  ],
+                  begin:  FractionalOffset(0.0, 0.0),
+                  end:  FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp,
+                )
+            ),
+          ),
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "loow",
+            style: TextStyle(
+                fontSize: 60,
+                color: Colors.white,
+                fontFamily: "Signatra",
+                letterSpacing: 6
+            ),
+          ),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.lock, color: Colors.white,),
+                text: "Login",
+              ),
+              Tab(
+                icon: Icon(Icons.person, color: Colors.white,),
+                text: "Register",
+              ),
+            ],
+            indicatorColor: Colors.white38,
+            indicatorWeight: 6,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Colors.amber,
+                  Colors.cyan,
+                ],
+              )
+          ),
+          child: const TabBarView(
+            children: [
+              LoginScreen(),
+              RegisterScreen(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
