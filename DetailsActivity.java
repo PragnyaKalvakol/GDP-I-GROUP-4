b09b3f307dd7cdaf24b2d5d75b7b825fb49b1ac3
Sreeja Kumbhum <98132897+ksreejareddy@users.noreply.github.com>
2023-03-17 11:00:08 -0500

    Update DetailsActivity.java
    Steps to create the new activity folder.

--- DetailsActivity.java
+++ DetailsActivity.java
@@ -1,4 +1,11 @@
-package com.tutlane.sharedpreferencesexample;
+create another activity file DetailsActivity.java in \java\com.tutlane.sharedpreferences path to show the details from shared preference file 
+for that 
+step:1 Right-click on your application folder 
+step:2 Go to New a select Java Class 
+step:3 give name as DetailsActivity.java.package com.loowapp.sharedpreferences;
+
+we create a new activity file DetailsActivity.java, open it and write the code 
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
