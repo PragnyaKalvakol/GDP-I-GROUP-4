5890d523aca5d406de2907dba612a4c5e9bd6786
Sreeja Kumbhum <98132897+ksreejareddy@users.noreply.github.com>
2023-03-17 11:01:13 -0500

    Update DetailsActivity.java

--- DetailsActivity.java
+++ DetailsActivity.java
@@ -1,8 +1,8 @@
-create another activity file DetailsActivity.java in \java\com.tutlane.sharedpreferences path to show the details from shared preference file 
+create another activity file DetailsActivity.java in \java\com.loow.sharedpreferences path to show the details from shared preference file 
 for that 
 step:1 Right-click on your application folder 
 step:2 Go to New a select Java Class 
-step:3 give name as DetailsActivity.java.package com.loowapp.sharedpreferences;
+step:3 give name as DetailsActivity.java.package com.loow.sharedpreferences;
 
 we create a new activity file DetailsActivity.java, open it and write the code 
 
